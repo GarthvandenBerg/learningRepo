e395bfb3891e8e71c85c9231e4ca8e24d1e9e06a
Garth_van_den_Berg <garth14707@gmail.com>
2023-04-13 11:13:13 +0200

    added add and subtract buttons

--- app.js
+++ app.js
@@ -16,27 +16,67 @@ for (let i = 0; i < elements.length; i++) {
 }
 
 // User purchases an item it should get stored into local storage to use in cart 
-function storeItem(keyword, item) {
-    localStorage.setItem(keyword, item);
-    console.log(localStorage.getItem(keyword) + "FromStorage");
-}
+// function storeItem(keyword, item) {
+//     localStorage.setItem(keyword, item);
+//     console.log(localStorage.getItem(keyword) + "FromStorage");
+// }
 
 // Gets Item name from label when button press using parent node
 function getProduct(e) {
     const product = e.srcElement.parentNode;
+    const buttonType = e.srcElement.value;
     const currentProductName = product.getElementsByClassName("item-label")[0].innerHTML;
     let currentPrice = 0;
+    let currentCount = product.getElementsByClassName("item-count")[0].innerHTML;
 
+    const productObj = {
+        Product: currentProductName,
+        Price: currentPrice,
+        Quantity: currentCount,
+        productDOM: product
+    };
+
+    //getproduct price
     for (const prod in products) {
         if (prod === currentProductName) {
             currentPrice = products[prod]
         }
     }
 
-    cart += {
-        currentProductName: currentPrice
+    //check if adding or subtracting from 
+    if (buttonType === "Add") {
+        addButton(productObj);
+    } else {
+        subtractButton(productObj);
     }
+
     // storeItem(currentProductName, currentPrice);
     console.log(currentPrice);
-    console.log(cart);
+}
+
+function addButton (prodObj) {
+    const productName = prodObj.Product;
+    const productCost = prodObj.currentPrice;
+    let quantity = prodObj.Quantity;
+
+    prodObj.productDOM.getElementsByClassName("item-count")[0].innerHTML = Number(quantity) +1;
+    // localStorage.setItem("Products", )
+}
+
+function subtractButton (prodObj) {
+    const productName = prodObj.Product;
+    const productCost = prodObj.currentPrice;
+    let quantity = prodObj.Quantity;
+
+    if (quantity > 0) {
+        prodObj.productDOM.getElementsByClassName("item-count")[0].innerHTML = Number(quantity) - 1;
+    } else {
+        alert ("Cant go less than 0");
+    }
+    // localStorage.setItem("Products", )
+}
+
+//takes the totals of each and then gives total for all items
+function calculateCost () {
+
 }
---
+++ cart.js
@@ -0,0 +1,17 @@
+class Cart {
+    // cart = document.getElementById("cart");
+
+    constructor (productArray) {
+        this.productArray = productArray;
+        
+    }
+    //function to add to cart 
+    
+    //function to get cart info
+    
+    //function to delete from cart 
+    
+    //display cart in div when button press
+
+    //update cart.
+}
